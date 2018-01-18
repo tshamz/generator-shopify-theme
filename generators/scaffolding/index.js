c05eb2417331869802486169644d2d317be73473
Tyler Shambora <tyler@theshamboras.com>
2018-01-18 12:27:11 -0800

    add generic liquid templates

--- generators/scaffolding/index.js
+++ generators/scaffolding/index.js
@@ -3,6 +3,41 @@ var Generator = require('yeoman-generator');
 var chalk = require('chalk');
 var mkdirp = require('mkdirp');
 
+var templates = {
+  config: [ 'settings_data.json', 'settings_schema.json' ],
+  layout: [ 'checkout.liquid', 'theme.liquid' ],
+  locales: [ 'en.default.json' ],
+  snippets: [
+    'external-scripts.liquid',
+    'inline-cart.liquid',
+    'open-graph-tags.liquid',
+    'twitter-card.liquid'
+  ],
+  ['templates/customers']: [
+    'account.liquid',
+    'activate_account.liquid',
+    'addresses.liquid',
+    'login.liquid',
+    'order.liquid',
+    'register.liquid',
+    'reset_password.liquid'
+  ],
+  templates: [
+    '404.liquid',
+    'article.liquid',
+    'blog.liquid',
+    'cart.contents.liquid',
+    'cart.liquid',
+    'collection.liquid',
+    'gift_card.liquid',
+    'index.liquid',
+    'list-collections.liquid',
+    'page.liquid',
+    'product.liquid',
+    'search.liquid'
+  ]
+}
+
 module.exports = class extends Generator {
   prompting () {
     this.log(chalk.red.bold('\nSCAFFOLDING DIRECTORY STRUCTURE\n'));
@@ -42,6 +77,15 @@ module.exports = class extends Generator {
       mkdirp('dev/liquid/sections');
       mkdirp('dev/liquid/snippets');
       mkdirp('dev/liquid/templates');
+      mkdirp('dev/liquid/templates/customers');
+      for (var directory in templates) {
+        templates[directory].forEach(function (template) {
+          this.fs.copy(
+            this.templatePath(template),
+            this.destinationPath('./dev/liquid/' + directory + '/' + template);
+          )
+        }, this);
+      }
     }
   }
 };
